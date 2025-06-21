45680b98cf5accf8a636f491749c177d71562467
Lmdingi <lmdingi4@gmail.com>
2025-06-21 16:32:25 +0200

    add create event ui basic info

---
+++ Services/Models/Event.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Models
+{
+    public class Event
+    {
+        // <!-- Event Title -->
+        [Required]
+        public string Title { get; set; } =  string.Empty;
+
+        // <!-- Event Date and Time -->
+        public string TimeZone { get; set; } = TimeZoneInfo.GetSystemTimeZones().ToList().First().DisplayName;
+
+        [Required]
+        [Display(Name = "Start Date")]
+        [FutureDate(ErrorMessage = "Start Date cannot be in the past.")]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime? EndDate { get; set; }
+
+        // <!-- Location Mode Selection -->
+        public bool IsRomote { get; set; }
+
+        public string? RemoteLink { get; set; }
+
+        public string? PhysicalLocation { get; set; }
+
+        //   <!-- Pricing Section -->
+        public string? Currency { get; set; }
+        public decimal Price { get; set; }
+        public string? TicketLink { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null) return true; 
+
+            DateTime dateValue = (DateTime)value;
+            return dateValue.Date >= DateTime.Today;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"{name} cannot be in the past.";
+        }
+    } 
+}
